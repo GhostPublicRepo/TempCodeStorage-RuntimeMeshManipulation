ac8c21e6aa83cb9fe944cd81f1b38f8ecbccaaa2
GhostPublicRepo <72549649+GhostPublicRepo@users.noreply.github.com>
2020-10-08 10:31:30 +0100

    Add files via upload

---
+++ ArrayExtensionMethod.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ArrayExtensionMethod
+{
+    public static List<int> RemoveAllSpecifiedIndicesFromArray(this int[] a, bool[] indicesToRemove)
+    {
+        List<int> b = new List<int>();
+        for (int i = 0; i < indicesToRemove.Length; ++i)
+        {
+            if (!indicesToRemove[i])
+                b.Add(a[i]);
+        }
+
+        return b;
+    }
+    public static List<int> IndexOf(this Array a, object o)
+    {
+        List<int> result = new List<int>();
+        for (int i = 0; i < a.Length; ++i)
+        {
+            if (a.GetValue(i).Equals(o))
+            {
+                result.Add(i);
+            }
+        }
+        result.Sort();
+        return result;
+    }
+}
